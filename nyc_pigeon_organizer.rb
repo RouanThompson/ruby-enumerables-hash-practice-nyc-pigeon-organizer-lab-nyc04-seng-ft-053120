7e4093279d8fdb33192144befafa9a7039858f7d
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-15 14:27:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,3 +7,18 @@ def nyc_pigeon_organizer(data)
 end
 
 nyc_pigeon_organizer(data)
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
