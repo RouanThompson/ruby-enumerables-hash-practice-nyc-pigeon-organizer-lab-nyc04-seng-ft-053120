bc6187b6375bcf7c8189ae2607ebe4e20be0d765
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-15 14:56:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each do |hash, (key, value)|
-    puts "#{hash} #{key}, #{value}"
+    puts "#{hash} => #{key}, #{value}"
     if key
   end
 end
