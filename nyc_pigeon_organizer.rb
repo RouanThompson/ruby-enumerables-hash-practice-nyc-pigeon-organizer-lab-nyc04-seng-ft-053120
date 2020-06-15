7eaac689c75c597fb1a6da1e46353e5a98a16c0b
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-15 14:15:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {
+    "Theo" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Peter Jr." => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Lucky" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Ms. K" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Queenie" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Andrew" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    },
+    "Alex" => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+  }
+  binding.pry
+  data
 end
