d94c255c1b5b65652f57da65e85091cd1d9fda65
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-15 20:31:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, key|
+    key.each do |value, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][attribute]
+          new_hash[name][attribute] = []
+        end
+        new_hash[name][attribute].push(value.to_s)
+      end
+    end
+  end
+  new_hash
 end
